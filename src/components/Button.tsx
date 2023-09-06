131ae6dee67a7733c9d1e34941b41a30e1b5d259
Michael0520 <po2319987@gmail.com>
2023-09-07 03:26:55 +0800

    feat: 🎸 add timer click event in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,3 +1,6 @@
+import { useTimer } from "../hook/useTimer";
+import { useMouseEvent } from "../hook/useMouseEvent";
+
 export enum ButtonAction {
   Increment = "INCREMENT",
   Decrement = "DECREMENT",
@@ -10,26 +13,29 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ disabled, action, onClick }) => {
-  let bgColor = "bg-blue-300 hover:bg-blue-400";
-  let textColor = "text-white";
+  const { startTimer, clearTimer } = useTimer(onClick, 1000);
 
-  if (action === ButtonAction.Increment) {
-    bgColor = "bg-blue-400 hover:bg-blue-500";
-  } else if (action === ButtonAction.Decrement) {
-    bgColor = "bg-blue-300 hover:bg-blue-400";
-  }
+  const mouseEvents = useMouseEvent(
+    () => {
+      startTimer();
+    },
+    () => {
+      clearTimer();
+    }
+  );
 
-  const buttonStyle = disabled
-    ? `border-blue-300 text-blue-300 opacity-50 cursor-not-allowed`
-    : `border-blue-800 ${bgColor} ${textColor}`;
+  const disableButtonStyle = disabled
+    ? "border-blue-300 text-blue-300 opacity-50 cursor-not-allowed"
+    : "border-blue-800 bg-blue-400 hover:bg-blue-500 text-white";
 
   const label = action === ButtonAction.Increment ? "+" : "-";
 
   return (
     <button
-      className={` select-none w-12 h-12 text-2xl border-2 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ${buttonStyle}`}
+      className={`select-none w-12 h-12 text-2xl border-2 rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 ${disableButtonStyle}`}
       onClick={onClick}
       disabled={disabled}
+      {...mouseEvents}
     >
       {label}
     </button>
