865489fa1f727638c0cf410f8755b592218fd21c
Michael0520 <po2319987@gmail.com>
2023-09-08 04:22:09 +0800

    feat: 🎸 update mobile touch event trigger without mouse

--- src/hook/useMouseAndTouchEvents.ts
+++ src/hook/useMouseAndTouchEvents.ts
@@ -1,30 +1,31 @@
 type MouseAndTouchEvents = {
-    onMouseDown: () => void;
-    onMouseUp: () => void;
-    onMouseLeave: () => void;
-    onTouchStart: () => void;
-    onTouchEnd: () => void;
-    onTouchCancel: () => void;
+    onMouseDown: (e: React.MouseEvent) => void;
+    onMouseUp: (e: React.MouseEvent) => void;
+    onMouseLeave: (e: React.MouseEvent) => void;
+    onTouchStart: (e: React.TouchEvent) => void;
+    onTouchEnd: (e: React.TouchEvent) => void;
+    onTouchCancel: (e: React.TouchEvent) => void;
 };
 
 export const useMouseAndTouchEvents = (
     mouseDownCallback: () => void,
     mouseUpCallback: () => void
 ): MouseAndTouchEvents => {
-    const commonStart = () => {
+
+    const commonStart = (e: Event) => {
         mouseDownCallback();
     };
 
-    const commonEnd = () => {
+    const commonEnd = (e: Event) => {
         mouseUpCallback();
     };
 
     return {
-        onMouseDown: commonStart,
-        onMouseUp: commonEnd,
-        onMouseLeave: commonEnd,
-        onTouchStart: commonStart,
-        onTouchEnd: commonEnd,
-        onTouchCancel: commonEnd,
+        onMouseDown: (e: React.MouseEvent) => commonStart(e.nativeEvent),
+        onMouseUp: (e: React.MouseEvent) => commonEnd(e.nativeEvent),
+        onMouseLeave: (e: React.MouseEvent) => commonEnd(e.nativeEvent),
+        onTouchStart: (e: React.TouchEvent) => commonStart(e.nativeEvent),
+        onTouchEnd: (e: React.TouchEvent) => { e.preventDefault(); commonEnd(e.nativeEvent); },
+        onTouchCancel: (e: React.TouchEvent) => { e.preventDefault(); commonEnd(e.nativeEvent); },
     };
 };
